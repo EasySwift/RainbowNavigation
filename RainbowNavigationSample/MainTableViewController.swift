a0bc98b938abd0daa19877c442c25ae99bd83fc7
stubbornnessness <1219721814yxj>
2016-10-09 23:47:58 +0800

    1.1.1版本支持swift3，支持carthage

--- RainbowNavigationSample/MainTableViewController.swift
+++ RainbowNavigationSample/MainTableViewController.swift
@@ -31,16 +31,16 @@ class MainTableViewController: UITableViewController, RainbowColorSource {
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
 
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("SampleCell", forIndexPath: indexPath)
+     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SampleCell", for: indexPath as IndexPath)
 
         switch indexPath.row {
         case 0:
@@ -54,12 +54,12 @@ class MainTableViewController: UITableViewController, RainbowColorSource {
         return cell
     }
 
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         switch indexPath.row {
         case 0:
-            performSegueWithIdentifier("PushColorVC", sender: self)
+            performSegue(withIdentifier: "PushColorVC", sender: self)
         case 1:
-            performSegueWithIdentifier("PushTransparentVC", sender: self)
+            performSegue(withIdentifier: "PushTransparentVC", sender: self)
         default:
             break
         }
